ee6304e7058c42bdd516e785c6c9fc6a943ed1ac
JacekDob <jacekdob@gmail.com>
2017-03-15 20:14:11 +0100

    Update cfg.js

--- cfg.js
+++ cfg.js
@@ -3,7 +3,11 @@ exports.mqttServer = { address: 'tcp://localhost', username: 'user', password: '
 exports.server = 1;
 
 // mapping
-exports.btNames = {};
+var btNames = {};
 
-exports.btNames['00:11:22:33:44:55'] = { name: 'eq3_device1', address: '00:11:22:33:44:55', server: 1 };
-exports.btNames['eq3_device1'] = exports.btNames['00:11:22:33:44:55'];
+exports.btNames['eq3_device1'] = { name: 'eq3_device1', address: '00:11:22:33:44:55', server: 1 };
+
+exports.btNames = btNames;
+
+// uncomment if there is prefix for mqtt name
+//exports.prefix = 'eq3_';
