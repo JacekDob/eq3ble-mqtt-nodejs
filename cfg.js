faf14cee04d1fda610df99c21430f2e4ba34d5b3
JacekDob <jacekdob@gmail.com>
2017-10-21 22:00:38 +0200

    Update cfg.js

--- cfg.js
+++ cfg.js
@@ -1,15 +1,21 @@
 // constants:
-exports.mqttServer = { address: 'tcp://localhost', username: 'user', password: 'pass' };
-
-// set if different than 1
+exports.mqttServer = { address: 'tcp://localhost', username: 'username', password: 'password' };
 // exports.server = 1;
+var servers = {};
+servers['192.168.1.11'] = 1;
+servers['192.168.1.12'] = 2;
 
 // mapping
 var btNames = {};
-
-btNames['eq3_device1'] = { name: 'eq3_device1', address: '00:11:22:33:44:55', server: 1 };
-
+btNames['eq3_device1'] = { name: 'eq3_device1', friendlyname: 'device 1', address: '00:11:22:33:44:55', server: 1 };
 exports.btNames = btNames;
 
-// uncomment if there is prefix for mqtt name
-// exports.prefix = 'eq3_';
+exports.prefix = 'eq3_';
+exports.serverChoiceMethod = 'manual';
+
+exports.scanFrequency = 3 * 60 * 60 * 1000;
+exports.scanStarted;
+exports.scanTimeoutTime = 15 * 1000;
+exports.connectTimeout = 15 * 1000;
+exports.retries = 1;
+exports.waitAfterFail = 5 * 60 * 1000;
