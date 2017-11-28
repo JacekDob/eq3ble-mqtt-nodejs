aebb2a2de4ea4d27d5ff98675cb17af16b581a7d
JacekDob <jacekdob@gmail.com>
2017-11-28 19:50:01 +0100

    Update interface.js

--- library/interface.js
+++ library/interface.js
@@ -27,8 +27,32 @@ var payload = exports.payload = {
   setManualMode: function setManualMode() {
     return new Buffer('4040', 'hex');
   },
-  setEcoMode: function setEcoMode() {
-    return new Buffer('4080', 'hex');
+  setVacationMode: function setVacationMode(endTime, setPointTemperature) { // == vacation
+
+/*
+    public SetVacationModeCommand(Calendar endTime, Number setPointTemperature) {
+        this.commandData = new byte[6];
+        this.commandData[0] = (byte) 64;
+        this.commandData[1] = Byte.MIN_VALUE;
+        byte[] bArr = this.commandData;
+        bArr[1] = (byte) (bArr[1] | ((byte) ((int) (setPointTemperature.doubleValue() * 2.0d))));
+        this.commandData[2] = (byte) endTime.get(5);
+        this.commandData[3] = (byte) (endTime.get(1) % 100);
+        this.commandData[4] = (byte) ((endTime.get(11) * 2) + (endTime.get(12) / 30));
+        this.commandData[5] = (byte) (endTime.get(2) + 1);
+    }
+*/
+    var b = Buffer.alloc(6);
+    b[0] = 0x40;
+    b[1] = setPointTemperature * 2;
+    b[2] = date.getDate(); // day
+    b[3] = (date.getFullYear() % 100);
+    b[4] = date.getHours() * 2 + date.getMinutes() / 30;
+    b[5] = (date.getMonth() + 1);
+
+    return b;
+
+    //return new Buffer('4080', 'hex');
   },
   lockThermostat: function lockThermostat() {
     return new Buffer('8001', 'hex');
@@ -40,19 +64,12 @@ var payload = exports.payload = {
     return new Buffer('41' + (temperature <= 7.5 ? '0' : '') + (2 * temperature).toString(16), 'hex');
   },
   setTemperatureOffset: function setTemperatureOffset(offset) {
-    console.log('Offset=', offset);
-    var b = Buffer.alloc(2);
-    b[0] = 19;
-    b[1] = 2 * offset + 7;
-    console.log('Buffer=', b);
-    return b;
-
-    //return new Buffer('13' + (2 * offset + 7).toString(16), 'hex');
+    return new Buffer('13' + (2 * offset + 7).toString(16), 'hex');
   },
-  setDay: function setDay() {
+  setDay: function setDay() { // == comfort
     return new Buffer('43', 'hex');
   },
-  setNight: function setNight() {
+  setNight: function setNight() { // == eco
     return new Buffer('44', 'hex');
   },
   setComfortTemperatureForNightAndDay: function setComfortTemperatureForNightAndDay(night, day) {
@@ -93,10 +110,13 @@ var payload = exports.payload = {
     b[1] = day;
     for (var i=0;i<periods.length && i<7;i++) {
       b[(i*2)+2] = periods[i].temperature * 2;
-      if (periods[i].to)
+      if (periods[i].toHHMM) {
+        var hhmm = periods[i].toHHMM.split(':');
+        var humanNumber = parseInt(hhmm[0]) + parseInt(hhmm[1]);
+        b[(i*2)+3] = humanNumber / 10;
+      } else if (periods[i].to) {
         b[(i*2)+3] = periods[i].to;
-      else if (periods[i].toHuman)
-        b[(i*2)+3] = periods[i].toHuman * 60 / 10;
+      }
     }
     return b;
   }
@@ -140,20 +160,32 @@ function parseProfile(buffer) {
   var profile = {};
   var periods = [];
   profile.periods = periods;
-  if (buffer[0] == 33) {  
+  if (buffer[0] == 33) {
     profile.dayOfWeek = buffer[1]; // 0-saturday, 1-sunday
     profile.dayOfWeekName = dayNames[profile.dayOfWeek];
     for (var i=2; i<buffer.length;i+=2) {
       if (buffer[i] != 0) {
           var temperature = (buffer[i] / 2);
 	  var to = buffer[i+1];
-          var toHuman = (buffer[i+1] * 10 /60);
+          var toHHMM = toHHMMString(to * 10 / 60);
 	  var from = periods.length == 0 ? 0 : periods[periods.length-1].to;
-	  var fromHuman = periods.length == 0 ? 0 : periods[periods.length-1].toHuman;
-          periods.push({ temperature: temperature, from: from, to: to, fromHuman: fromHuman, toHuman: toHuman });
+	  var fromHHMM = periods.length == 0 ? '0:00' : periods[periods.length-1].toHHMM;
+          periods.push({ idx: 'idx'+(i/2-1), temperature: temperature, from: from, to: to, fromHHMM: fromHHMM, toHHMM: toHHMM });
       }
+    }
+    for (var i=0; i<profile.periods.length;i++) {
+//       delete profile.periods[i].from;
+//       delete profile.periods[i].to;
     }
   }  
   return profile;
 }
 
+function toHHMMString(f) {
+	var rounded = Math.floor(f);
+	var rest = f - rounded;
+	var h = rounded;
+	var m = Math.round(rest * 60);
+	var mm = m < 10 ? '0' + m : m;
+	return h + ':' + mm;
+}
